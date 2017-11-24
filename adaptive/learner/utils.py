57f286a0969f808fc9ff1562ee29f1a435d679d3
Bas Nijholt <basnijholt@gmail.com>
2017-11-24 12:46:18 +0100

    add a blocking learner runner

--- adaptive/learner/utils.py
+++ adaptive/learner/utils.py
@@ -10,3 +10,25 @@ def restore(*learners):
     finally:
         for state, learner in zip(states, learners):
             learner.__setstate__(state)
+
+
+def run_learner(learner, n_points=1):
+    """Evaluate the learner's function n_points times.
+
+    Run the learner's `choose_points` and `add_point` methods
+    while blocking the kernel. For example; this is useful to
+    easily extract error messages.
+
+    Parameters
+    ----------
+    learner : adaptive.BaseLearer object
+        A learner that as choose_points, add_point, and function
+        methods.
+    n_points : int
+        Number of points to evaluate.
+    """
+    for _ in range(n_points):
+        xs, _ = learner.choose_points(1)
+        for x in xs:
+            y = learner.function(x)
+            learner.add_point(x, y)
